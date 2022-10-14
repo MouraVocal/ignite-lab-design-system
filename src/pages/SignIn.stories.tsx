a5820b8617fe95c661c0485db9dd05a4570b00bc
mouravocal <diego.moura3@gmail.com>
2022-10-14 18:12:16 -0300

    feat: storybook tests

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { within, userEvent, waitFor } from '@storybook/testing-library'
+import { expect } from '@storybook/jest'
+import { SignIn } from './SignIn'
+
+export default {
+  title: 'Pages/SignIn',
+  component: SignIn,
+  args: {},
+  argTypes: {}
+} as Meta
+
+export const Default: StoryObj = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement)
+
+    userEvent.type(
+      canvas.getByPlaceholderText('johndoe@example.com'),
+      'diego.moura3@gmail.com'
+    )
+    userEvent.type(canvas.getByPlaceholderText('*************'), '1234568478')
+    userEvent.click(canvas.getByRole('button'))
+
+    await waitFor(() => {
+      return expect(canvas.getByText('Login realizado!')).toBeInTheDocument()
+    })
+  }
+}
